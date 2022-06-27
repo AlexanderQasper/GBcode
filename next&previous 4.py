aca168599dbc2076f5353352b4393c3a8857d9fe
Alex QAsper <semidog@gmail.com>
2022-06-27 21:48:53 +0300

    123123

---
+++ next&previous 4.py
@@ -0,0 +1,6 @@
+number = int(input())
+nextnumber = number +1
+prevnumber = number -1
+
+print("The next number for the number", number, "is", nextnumber)
+print("The previous number for the number", number, "is", prevnumber)
